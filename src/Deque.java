ce4a2a997fae566669182317e96805b4d3d71a15
William Grasel <d.rufos@gmail.com>
2013-02-25 17:47:47 -0300

    Inserindo primeiros testes de Exception

--- src/Deque.java
+++ src/Deque.java
@@ -15,19 +15,23 @@ public class Deque<Item> implements Iterable<Item> {
     }
     
     // insert the item at the front
-    public void addFirst(Item item){}
+    public void addFirst(Item item){
+        if(item==null) throw new NullPointerException("Can't add a null Item.");
+    }
     
     // insert the item at the end
-    public void addLast(Item item){}
+    public void addLast(Item item){
+        if(item==null) throw new NullPointerException("Can't add a null Item.");
+    }
     
     // delete and return the item at the front
     public Item removeFirst(){
-        return null;
+        throw new java.util.NoSuchElementException("Deque is empty.");
     }
     
     // delete and return the item at the end
     public Item removeLast(){
-        return null;
+        throw new java.util.NoSuchElementException("Deque is empty.");
     }
     
     // return an iterator over items in order from front to end
--- test/DequeTest.java
+++ test/DequeTest.java
@@ -14,11 +14,60 @@ import static org.junit.Assert.*;
  */
 public class DequeTest {
     
+    Deque<String> subject;
+    
     @Before
     public void setUp() {
+        subject = new Deque<>();
     }
 
     @Test
-    public void testSomeMethod() {
+    public void deveMarcarComoVazioAoIniciar() {
+        assertTrue(subject.isEmpty());
+    }
+
+    @Test
+    public void deveTerTamanhoZeroAoIniciar() {
+        assertEquals(0, subject.size());
+    }
+
+    @Test
+    public void deveEstourarNullPointerExceptionAoAdicionarItemNuloAFrente() {
+        try{
+            subject.addFirst(null);
+            fail("Exception not thrown");
+        }catch(NullPointerException e){
+            assert true;
+        }
+    }
+
+    @Test
+    public void deveEstourarNullPointerExceptionAoAdicionarItemNuloATras() {
+        try{
+            subject.addLast(null);
+            fail("Exception not thrown");
+        }catch(NullPointerException e){
+            assert true;
+        }
+    }
+
+    @Test
+    public void deveEstourarNoSuchElementExceptionAoRemoverItemDaFrenteQuandoEstaVazio() {
+        try{
+            subject.removeFirst();
+            fail("Exception not thrown");
+        }catch(java.util.NoSuchElementException e){
+            assert true;
+        }
+    }
+
+    @Test
+    public void deveEstourarNoSuchElementExceptionAoRemoverItemDeTrasQuandoEstaVazio() {
+        try{
+            subject.removeLast();
+            fail("Exception not thrown");
+        }catch(java.util.NoSuchElementException e){
+            assert true;
+        }
     }
 }
